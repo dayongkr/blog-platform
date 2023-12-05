4c5da16b26ceca346c853151485e7ae8a0569a7a
dayongkr <dayongkr@gmail.com>
2023-12-05 16:46:59 +0900

    feat(fe): signup page & add logo

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,23 +1,31 @@
-"use client";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Label } from "@radix-ui/react-label";
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
 import Logo from "@/public/bookmark.png";
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <div className="flex w-full justify-center items-center min-h-screen">
       <div className="flex border-2 border-gray-100 rounded-xl p-10 flex-col gap-8 text-center">
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex gap-2 flex-col">
           <Link href="/">
             <Image src={Logo} width={30} height={30} alt="logo" />
           </Link>
-          <h1 className="text-3xl font-bold px-10">Welcome back!</h1>
-          <p className="text-sm text-gray-400">Please enter your details</p>
+          <h1 className="text-3xl font-bold text-left">Sign up</h1>
+          <p className="text-sm text-gray-400 text-left">
+            Please enter your details below to create your account and get
+            started.
+          </p>
         </div>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3 items-center">
+          <div className="w-full flex flex-col">
+            <Label className="text-left text-sm" htmlFor="name">
+              Name
+            </Label>
+            <Input placeholder="Your nickname" id="name" />
+          </div>
           <div className="w-full flex flex-col">
             <Label className="text-left text-sm" htmlFor="email">
               Email
@@ -30,11 +38,11 @@ export default function SignIn() {
             </Label>
             <Input placeholder="Password" id="Password" type="password" />
           </div>
-          <Button className="w-full rounded-full mt-3">Sign in</Button>
+          <Button className="w-full rounded-full mt-3">Sign Up</Button>
           <p className="text-sm text-gray-400">
-            Don&rsquo;t have an account?{" "}
-            <Link className="text-gray-600 hover:underline" href={"signup"}>
-              Sign Up
+            Already have an account?{" "}
+            <Link className="text-gray-600 hover:underline" href={"signin"}>
+              Sign In
             </Link>
           </p>
         </div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { Button } from "./ui/button";
 import { useRouter, usePathname } from "next/navigation";
+import Image from "next/image";
+import Logo from "@/public/bookmark.png";
+import Link from "next/link";
 
 export default function Nav() {
   const router = useRouter();
@@ -8,9 +11,15 @@ export default function Nav() {
   console.log(pathname);
   return (
     <>
-      {pathname === "/signin" ? null : (
+      {pathname === "/signin" || pathname === "/signup" ? null : (
         <nav className="flex justify-between items-center px-5 py-3">
-          <h1 className="font-bold text-lg">Blog Platform</h1>
+          <Link
+            href="/"
+            className="flex w-fit justify-start items-center gap-1"
+          >
+            <Image src={Logo} alt="logo" width={20} />
+            <h1 className="font-bold text-lg">Blog Platform</h1>
+          </Link>
           <Button
             className="rounded-full py-1 px-3 h-7"
             onClick={() => router.push("signin")}
